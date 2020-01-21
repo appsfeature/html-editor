ba9c8714069bf7933786947cbd65f65c7cb9abb1
helios <alphatestingdroid@gmail.com>
2020-01-21 16:23:03 +0530

    Migrate to AndroidX

--- app/src/main/java/com/htmleditor/sample/TestActivity.java
+++ app/src/main/java/com/htmleditor/sample/TestActivity.java
@@ -1,8 +1,8 @@
 package com.htmleditor.sample;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -17,7 +17,7 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         setupToolBar();
-        htmlTextEditor = (HtmlTextEditor) findViewById(R.id.summernote);
+        htmlTextEditor = (HtmlTextEditor) findViewById(R.id.html_editor);
     }
 
     @Override
